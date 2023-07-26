a98eca8163b8a2f3da1104f338692ab6c2e85560
Kartman570 <kotelnikovilya@yandex.ru>
2023-07-26 14:54:45 +0400

    Quotes correct. Linter ok

--- flask_exercises/flask_excercises.py
+++ flask_exercises/flask_excercises.py
@@ -6,34 +6,34 @@ class FlaskExercise:
 
     @staticmethod
     def configure_routes(app: Flask) -> None:
-        @app.route('/user', methods=['POST'])
+        @app.route("/user", methods=["POST"])
         def create_user():
             data = request.get_json()
-            if 'name' not in data:
+            if "name" not in data:
                 return make_response(jsonify({"errors": {"name": "This field is required"}}), 422)
-            FlaskExercise.users[data['name']] = data
+            FlaskExercise.users[data["name"]] = data
             return make_response(jsonify({"data": f"User {data['name']} is created!"}), 201)
 
-        @app.route('/user/<name>', methods=['GET'])
+        @app.route("/user/<name>", methods=["GET"])
         def get_user(name: str):
             user = FlaskExercise.users.get(name)
             if not user:
                 return make_response(jsonify({"error": "User not found"}), 404)
             return make_response(jsonify({"data": f"My name is {name}"}), 200)
 
-        @app.route('/user/<name>', methods=['PATCH'])
+        @app.route("/user/<name>", methods=["PATCH"])
         def update_user(name: str):
             data = request.get_json()
-            if 'name' not in data:
+            if "name" not in data:
                 return make_response(jsonify({"errors": {"name": "This field is required"}}), 422)
             if name not in FlaskExercise.users:
                 return make_response(jsonify({"error": "User not found"}), 404)
-            FlaskExercise.users[data['name']] = data
+            FlaskExercise.users[data["name"]] = data
             return make_response(jsonify({"data": f"My name is {data['name']}"}), 200)
 
-        @app.route('/user/<name>', methods=['DELETE'])
+        @app.route("/user/<name>", methods=["DELETE"])
         def delete_user(name: str):
             if name not in FlaskExercise.users:
                 return make_response(jsonify({"error": "User not found"}), 404)
             del FlaskExercise.users[name]
-            return '', 204
+            return "", 204
